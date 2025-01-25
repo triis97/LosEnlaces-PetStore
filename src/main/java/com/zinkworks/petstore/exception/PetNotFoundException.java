4429cdb3fdc05dac70b1500a4ff9c101fefa0e82
Triis97 <tristangm.dev@gmail.com>
2025-01-25 10:00:25 +0000

    Prepare starter branch with project structure

--- src/main/java/com/zinkworks/petstore/exception/PetNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.zinkworks.petstore.exception;
-
-public class PetNotFoundException extends RuntimeException{
-    public PetNotFoundException(final String documentId){
-        super("Pet with id %s was not found".formatted(documentId));
-    }
-}
