73f411fd557807d81904aad3c677afa3b453cfeb
Triis97 <tristangm.dev@gmail.com>
2025-02-04 19:09:31 +0000

    Add applicationConfig

---
+++ src/main/java/com/zinkworks/petstore/configuration/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.zinkworks.petstore.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //Anotacion para que SpringBoot reconozca esta clase como una clase especial de configuracion
+public class ApplicationConfig {
+
+}
